6b757949a033127f412ebb3cec221fb0555fc691
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-09-04 11:06:37 -0400

    make more changes to the portfolio

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Guillermo O. Hasbun Jr.',
+    site_url: 'https://www.ghasbun.dev/',
+    feed_url: 'https://www.ghasbun.dev/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
