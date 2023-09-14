a5e012f3fe58f6582c41ced5bf4a16ff53ed89af
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-09-14 14:51:29 -0400

    add modals

---
+++ modalThing.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+
+export function ModalThing(props: any) {
+  let subtitle: any;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = 'black';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <img src={props.pic} width='300' height='200' onClick={openModal}></img>
+      <Modal
+      ariaHideApp={false}
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <img src={props.pic} width='700' height='1050' ></img>
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>{props.words}</h2>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
+    </div>
+  );
+}
