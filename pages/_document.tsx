e372562d929617c56529ce51ae843d0cb605660a
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2024-10-29 15:04:11 -0400

    title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   const meta = {
     title: 'Raphael Bruce Shogreen-Hasbun - A candiate we can Trust',
-    description: 'Your Next TMA Treasurer',
+    description: 'Your Next TMA Secretary',
     // image: 'https://images.unsplash.com/photo-1607799279861-4dd421887fb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNvZnR3YXJlJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
   }
 
